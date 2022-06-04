730db9782ab96b86c20b22f83a7db87fb6ed58dd
Enzo Gama <enzo.betabit@gmail.com>
2022-06-04 20:28:32 -0300

    Inputs e data bind

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 const MyNameApp = {
     data() {
         return {
-            name: "Enzo"
+            name: "",
+            age: 20,
+            input_name:""
+        }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault();
+            this.name = this.input_name;
+            console.log(this.input_name);
         }
     },
 }
