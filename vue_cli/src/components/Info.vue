da8e9279408b5d889be9e19aa591309b01796cce
Enzo Gama <enzo.betabit@gmail.com>
2022-06-07 11:57:45 -0300

    diretivas v-if v-show e v-for

--- vue_cli/src/components/Info.vue
+++ vue_cli/src/components/Info.vue
@@ -1,20 +1,30 @@
 <template>
    <div>
-        <p>
+        <p v-if="esta_trabalhando">
             Estou trabalhando no momento
+        </p>
+        <p v-else>
+            Não estou trabalhando
         </p>
         <ul>
             <li>JS</li>
             <li>PHP</li>
             <li>Pitao</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{email}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name : 'Info'
-
+        name : 'Info',
+        data() {
+            return {
+                esta_trabalhando: true,
+                mostrar_email: true,
+                email: 'gamaenzosw@hotmail.com'
+            }
+        },
     }
 
 </script>
