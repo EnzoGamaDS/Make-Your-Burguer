134d585b72ffa23eb6c830d1f3cf67188886ff2e
Enzo Gama <enzo.betabit@gmail.com>
2022-06-10 17:28:30 -0300

    v-for

--- vue_cli/src/components/Info.vue
+++ vue_cli/src/components/Info.vue
@@ -6,10 +6,15 @@
         <p v-else>
             Não estou trabalhando
         </p>
+        <p>utilizo as seguintes tecs para back end </p>
         <ul>
-            <li>JS</li>
-            <li>PHP</li>
-            <li>Pitao</li>
+            <li v-for="(tec, index) in backend_tecs" v-bind:key="index">{{tec}}</li>
+        </ul>
+        <p>utilizo as seguintes tecs para front end </p>
+        <ul>
+            <li v-for="tec in frontend_tecs" :key="tec.id">
+                {{tec.language}}
+            </li>
         </ul>
         <div>
             <button @click="showEmail">{{textoBotao}}</button>
@@ -33,7 +38,13 @@ import Picture from './Picture.vue'
             mostrar_email: false,
             email: "gamaenzosw@hotmail.com",
             meu_link: "https://google.com",
-            textoBotao: "Mostrar email"
+            textoBotao: "Mostrar email",
+            backend_tecs: ['PHP', 'JS', 'Java'],
+            frontend_tecs: [
+                {id: 1,language:'HTML'},
+                {id: 2,language:'CSS'},
+                {id: 3,language:'JavaScript'},
+            ]
         };
     },
     methods: {
