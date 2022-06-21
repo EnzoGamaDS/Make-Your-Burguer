d4e625fd7cc9e8eb01882ec5a432311fb07617e1
Enzo Gama <enzo.betabit@gmail.com>
2022-06-21 12:20:58 -0300

    Multiple events with onclick.

---
+++ vue_cli/src/components/MultiplosEventos.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1, $event), segundo(txt2, $event), terceiro()"> Ativar multiplos eventos</button>
+        <p>{{multiplosEventos}}</p>
+    </div>    
+</template>
+
+<script>
+    export default{
+        name: 'Multiplos eventos',
+        data() {
+            return {
+                multiplosEventos: "",
+                txt1: "primeiro evento",
+                txt2: "segundo evento"
+            }
+        },
+        methods:{
+            primeiro(txt , e){
+                this.multiplosEventos = txt ;
+            },
+             segundo(txt , e){
+                setTimeout(() => this.multiplosEventos = txt, 500);
+            },
+             terceiro(){
+                setTimeout(() => this.multiplosEventos = "", 1500);
+            }
+        }
+    }
+    
+</script>
+
+<style scoped>
+    div{
+        margin: 100px;
+    }
+
+</style>>
--- vue_cli/src/components/PrimeiroComponente.vue
+++ vue_cli/src/components/PrimeiroComponente.vue
@@ -2,14 +2,21 @@
     <div>
         <h1>Olá vue</h1>
         <p>Meu nome é {{nome}}</p>
+        <MultiplosEventos/>
     </div>
 </template>
 
 <script>
+    import MultiplosEventos from './MultiplosEventos.vue'
     export default {
+        name:"PrimeiroComponente",
+        components: {
+            MultiplosEventos
+        },
         data(){
             return{
-                nome: "Enzo"
+                nome: "Enzo",
+                profissao: "programador"
             }
         }
     }
