f9bb8dfa2eedc056edb3a27107a773dbefa84185
Enzo Gama <enzo.betabit@gmail.com>
2022-06-21 11:09:21 -0300

    Click e submit

--- vue_cli/src/App.vue
+++ vue_cli/src/App.vue
@@ -3,6 +3,7 @@
     <PrimeiroComponente/>
     <HelloWorldVue/>
     <PessoaVue/>
+    <FormVue/>
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import PrimeiroComponente  from './components/PrimeiroComponente.vue'
 import HelloWorldVue from './components/HelloWorld.vue'
 import PessoaVue from './components/Pessoa.vue'
+import FormVue from './components/Form.vue'
 export default {
   name : 'App',
   components : {
     PrimeiroComponente,
     HelloWorldVue,
-    PessoaVue
+    PessoaVue,
+    FormVue
   }
 }
 </script>
---
+++ vue_cli/src/components/Form.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <form action="" @submit="enviarFormulario($event)">
+            <div><input type="text" v-model="name"></div> 
+            <div><input type="text" v-model="email"></div> 
+            <div><InputTextVue/></div> 
+            <input type="submit" class="button">
+        </form>
+
+        <p>Aprendendo VueJS </p>
+        <p>Aprendendo VueJS </p>
+        <p>Aprendendo VueJS </p>
+   
+    </div>    
+</template>
+<script>
+import InputTextVue from './Form/InputText.vue'
+    export default {
+        components : {
+            InputTextVue
+        },
+        data(){
+            return {
+                name: "Enzo",
+                email:"gamaenzo@hotmail.com"
+            }
+        },
+        methods: {
+         enviarFormulario(e){
+            e.preventDefault();
+            const name = this.name;
+            const email = this.email;
+            console.log("O nome é "+name);
+            console.log("O email é "+email);
+         }   
+        },
+    }
+  
+</script>
+
+<style scoped>
+    .button {
+        width: 5vw;
+        height: 4vh;
+    }
+    p{
+        margin:150px;
+    }
+
+</style>
+
