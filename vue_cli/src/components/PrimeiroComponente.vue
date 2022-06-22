82ccc9062b4211bc45a99f0aa46b9cc7218201bf
Enzo Gama <enzo.betabit@gmail.com>
2022-06-22 12:02:22 -0300

    reuse components with vue

--- vue_cli/src/components/PrimeiroComponente.vue
+++ vue_cli/src/components/PrimeiroComponente.vue
@@ -3,15 +3,19 @@
         <h1>Olá vue</h1>
         <p>Meu nome é {{nome}}</p>
         <MultiplosEventos/>
+        <ReutilizacaoVue/>
     </div>
 </template>
 
 <script>
     import MultiplosEventos from './MultiplosEventos.vue'
+    import ReutilizacaoVue from './Reutilizacao.vue'
+
     export default {
         name:"PrimeiroComponente",
         components: {
-            MultiplosEventos
+            MultiplosEventos,
+            ReutilizacaoVue
         },
         data(){
             return{
---
+++ vue_cli/src/components/Reutilizacao.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="Ativar">CLicke aqui !</button>
+        <span v-show="ativou">Ativado</span>
+    </div>
+</template>
+
+<script>
+    export default {
+    name:'Reutilizacao',
+    data() {
+        return {
+            ativou: false      
+        }
+    },
+    methods: {
+        Ativar() {
+            this.ativou = !this.ativou;
+        }
+        }
+    }
+
+</script>
