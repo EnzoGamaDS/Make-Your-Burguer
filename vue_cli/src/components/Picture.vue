a1a9b2389a0c91eb32a41bf805239199d662070b
Enzo Gama <enzo.betabit@gmail.com>
2022-06-23 07:57:16 -0300

    Usando emit para trocar imagem.

--- vue_cli/src/components/Picture.vue
+++ vue_cli/src/components/Picture.vue
@@ -1,15 +1,29 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem"/>
+    </div>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue'
+
 export default {
     name: 'Picture',
+    components: {
+        MudarImagem
+    },
     data() {
         return {
             avatar: "./images/avatar.png",
             descricao: "Enzo Gama"
         }
     },
+    methods:{
+        trocarImagem(){
+            console.log("Cheguei aqui")
+            this.avatar = './images/foto1.jpg'
+        }
+    }
 }
 </script>
