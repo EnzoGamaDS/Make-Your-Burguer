9d3dd701e301af54e31d289c186add991758344f
Enzo Gama <enzo.betabit@gmail.com>
2022-06-25 14:51:56 -0300

    Estilizando header e footer

--- your_burguer/src/components/Footer.vue
+++ your_burguer/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-    <p>Footer</p>
-    </div>
+    <footer id="footer">
+    <p>Make your burguer &copy; 2022</p>
+    </footer>
 </template>
 
 <script>
@@ -9,3 +9,17 @@
         name: "Footer"
     }
 </script>
+
+<style scoped>
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+    }
+</style>
