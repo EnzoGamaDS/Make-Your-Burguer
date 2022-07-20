0deceecb4865548b522217de4c687ed51fefdb45
Enzo Gama <enzo.betabit@gmail.com>
2022-07-20 10:14:45 -0300

    Recebendo status do banco

--- your_burguer/src/components/Dashboard.vue
+++ your_burguer/src/components/Dashboard.vue
@@ -26,6 +26,9 @@
                 <div>
                     <select name="status" class="status">
                         <option value="">Selecione</option>
+                        <option value="s.tipo" v-for="s in status" :key="s.id" :selected="burger.status == s.tipo">
+                        {{s.tipo}}
+                        </option>
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -55,6 +58,15 @@ export default {
             console.log(this.burgers);
 
             //resgatar os status
+            this.getStatus();
+
+        },
+        async getStatus(){
+            const req = await fetch("http://localhost:3000/status");
+
+            const data = await req.json();
+
+            this.status = data;
 
         }
     },
